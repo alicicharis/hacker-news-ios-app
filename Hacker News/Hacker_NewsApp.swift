f89a7d2bdcd02be07f7f423404779d1ac3e27894
Haris Alicic <haris.alicic1@gmail.com>
2025-01-18 10:27:38 +0100

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Haris Alicic on 18. 1. 2025..
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
